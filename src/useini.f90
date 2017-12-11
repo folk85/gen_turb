fef4242c9c4cf6e098503d7596cbfe0354648731
Sergey Medvedev <medvsn@gmail.com>
2017-12-11 14:34:26 +0300

    Set initial conditions and Update the array F(1:nface) and FB(1:nbfac) before every iteration.\n The problem is in Periodic boundary condition.

---
+++ src/useini.f90
@@ -0,0 +1,190 @@
+!=======================================================================
+!> @brief Special purpose routine for initialisation based on user coding
+!> @details All user-specified expressions must be written in standard
+!! Fortran 90 or in the Fortran version available on your machine. <br>
+!! To activate the statements, please remove the 'c' from the
+!! first column. <br>
+!! See the list of variables for the use of this routine.
+      SUBROUTINE useini(k,mph)
+!=======================================================================
+!.....contact fire@avl.com
+!-----
+!-----------------------------------------------------------------------
+!-----
+      USE comm0
+      USE comm1
+      
+      USE fire_mpi_var
+      
+      USE tmp_mod
+
+      IMPLICIT NONE
+!-----PARAMETER
+      INTEGER K
+      INTEGER MPH
+
+      real(prec) :: dlt !< Time duration
+      real(prec) :: dlx !< Length by X-corrdinate
+      real(prec) :: dly !< Length by Y-corrdinate
+      real(prec) :: dlz !< Length by Z-corrdinate
+      integer :: nt !< number of elements by time
+      integer :: nx !< number of elements by X-corrdinate
+      integer :: ny !< number of elements by Y-corrdinate
+      integer :: nz !<@var number of elements by Z-corrdinate
+      integer :: mat !< Number of material
+      real(prec) :: dsigma    !< define Deviation of velocities
+      real(prec) :: dlength   !< define Integral Length Scale 
+      real(prec) :: dtau      !< define Integral Time Scale 
+      real(prec) :: dstd         !<
+      real(prec) :: dmean         !<
+      real(prec) :: dcell         !<
+      real(prec) :: dtmp         !<
+      integer, dimension(3)  :: nels            !< 
+      real(prec), dimension(3)  :: dels         !<
+      real(prec), dimension(3)  :: std_i         !<
+      real(prec), dimension(3)  :: dmean_i         !<
+      real(prec), dimension(3)  :: dxx         !<
+      real(prec), dimension(3)  :: dvels         !<
+      CHARACTER(len=256) :: txt1,txt2
+      integer :: i,j
+!-----
+!-----------------------------------------------------------------------
+!-----
+      mat = 1
+
+      IF(I_USEINI == 1) THEN
+
+        if (itst .LT. 2) then
+          ! set  time duration
+          dlt = 2.0d-3
+          ! set space Length
+          dlx = 1.0d-2
+          dly = dlx*2.
+          dlz = dlx
+          ! set number of nodes
+          nx = 50
+          ny = nx*2
+          nz = nx
+
+          !set number of timesteps
+          nt = 2000
+          ntimes = nt
+
+          !set number of Modes
+          nmodes = 100
+          tcell  = nx * ny * nz
+          CALL tmp_alloc()
+          
+
+          !set the Integral values
+          dlength = 7.0d-3
+          dsigma = 2.0d+0
+          dtau = dlength / dsigma
+
+          !generate arrays 
+          dels(1) = dlx
+          dels(2) = dly
+          dels(3) = dlz
+
+          nels(1) = nx
+          nels(2) = ny*2
+          nels(3) = nz
+
+          write(*,*) "work in 3D-space + Time"
+
+          if (mpi_master) then
+            do i = 1, ntimes
+              in_time = nmodes * (i-1) + 1
+            ! generate fields
+              CALL gen_flow_3d(dels, nels, dsigma, dlength, dtau)
+              write(*,'(2(a,i))') "USEINI: gen_flow_3d:",i,'/',ntimes
+            end do
+          endif
+
+          i = ntimes * nmodes
+          if (mode_mpi) THEN
+            if (mpi_master) write(*,*) "CALL dbcast ac_m"
+            CALL dbcast(0, ac_m(1,1), 3*i)
+            if (mpi_master) write(*,*) "CALL dbcast as_m"
+            CALL dbcast(0, as_m(1,1), 3*i)
+            if (mpi_master) write(*,*) "CALL dbcast b_m"
+            CALL dbcast(0, b_m(1,1), 3*i)
+            if (mpi_master) write(*,*) "CALL dbcast c_m"
+            CALL dbcast(0, c_m(1), i)
+          END IF
+
+          ! if (mpi_master) write(*,*) "CALL dglscatter ac_m"
+          ! CALL dglscatter(ac_m,ac_m,3*i)
+          ! if (mpi_master) write(*,*) "CALL dglscatter as_m"
+          ! CALL dglscatter(as_m,as_m,3*i)
+          ! if (mpi_master) write(*,*) "CALL dglscatter b_m"
+          ! CALL dglscatter(b_m,b_m,3*i)
+          ! if (mpi_master) write(*,*) "CALL dglscatter c_m"
+          ! CALL dglscatter(c_m,c_m,i)
+          ! if (mpi_master) write(*,*) "CALL dglscatter cs_m"
+          ! CALL dglscatter(cs_m,cs_m,i)
+          ! if (mpi_master) write(*,*) "CALL dglscatter dphi_m"
+          ! CALL dglscatter(dphi_m,dphi_m,3*i)
+          if (mpi_master) write(*,*) "Check "
+
+          dxx(1) = 5.0d-5 ; dxx(2) = 5.0d-5 ; dxx(3) = 5.0d-5 
+          dvels(:) = 0.0d0
+          CALL set_vels_at_space_time(time,dxx,dvels)
+          ! write(txt1,'(i,s,3es10.2)') iampro,": vels ",dvels(1),dvels(2),dvels(3)
+          write(txt1,'(a,4es13.5)')" return First  ",dxx(1), dvels(1:3)
+          txt2=''
+          ! IF(iampro<2) CALL UCMESS('usedef','F',txt1,txt2)
+        
+          CALL FIO_UUMESS ('USEINI','I',txt1,txt2)
+
+          dxx(1) = 5.0d-5+dlx ; dxx(2) = 5.0d-5 ; dxx(3) = 5.0d-5 
+          dvels(:) = 0.0d0
+          CALL set_vels_at_space_time(time,dxx,dvels)
+          ! write(txt1,'(i,s,3es10.2)') iampro,": vels ",dvels(1),dvels(2),dvels(3)
+          write(txt1,'(a,4es13.5)')" return Second ",dxx(1), dvels(1:3)
+          txt2=''
+          ! IF(iampro<2) CALL UCMESS('usedef','F',txt1,txt2)
+          ! write(txt1,'(s)') "vels "!,dxx(1) !, dvels(1)
+          ! write(txt2,'(s,i,l)') "Use MPI mode",iampro, mpi_slave
+          CALL FIO_UUMESS ('USEINI','I',txt1,txt2)
+          dxx(1) = 5.0d-5 ; dxx(2) = 5.0d-5 ; dxx(3) = 5.0d-5 
+          dvels(:) = 0.0d0
+          CALL set_vels_at_space_time(time+dlt,dxx,dvels)
+          ! write(txt1,'(i,s,3es10.2)') iampro,": vels ",dvels(1),dvels(2),dvels(3)
+          write(txt1,'(a,4es13.5)')" return First  ",dxx(1), dvels(1:3)
+          txt2=''
+          ! IF(iampro<2) CALL UCMESS('usedef','F',txt1,txt2)
+        
+          CALL FIO_UUMESS ('USEINI','I',txt1,txt2)
+          ! write(*,*)txt1
+          CALL CFD_STOP()
+        endif
+
+        ! Define velocities
+        CALL set_vels_at_time(time)
+
+        ! write(*,*) "Calcs mean and deviation"
+        do i=1, 3
+          dmean = SUM(u(i,nsp(mat):nep(mat))) !/ DBLE(nep(mat)-nsp(mat)+1)
+          dcell = DBLE(nep(mat)-nsp(mat)+1)
+          CALL DGLSUM(dmean)
+          CALL DGLSUM(dcell)
+          dstd  = SUM((u(i,nsp(mat):nep(mat))-dmean)**2) !/ DBLE(nep(mat)-nsp(mat)+1)
+          CALL DGLSUM(dstd)
+          if (mpi_master) THEN
+            dtmp = SQRT((dstd-dmean**2)/dcell)
+            write(txt1,'(a,i13,2es13.5)') "Mean and std vel: ",i, dmean/dcell, dtmp
+            txt2 = ''
+            CALL FIO_UUMESS ('USEINI','I',txt1,txt2)
+          END IF ! (mpi_master) THEN
+        enddo
+
+!-----
+      ELSE IF(I_USEINI == 2) THEN
+!-----
+      ELSE IF(I_USEINI == 99) THEN
+         CALL USE_FORMULA('USEINI', 2, K, MPH, 0)
+      END IF
+!-----
+      RETURN
+      END
---
+++ src/useite.f90
@@ -0,0 +1,69 @@
+!=======================================================================
+!> @brief Special purpose routine for changing the run parameters
+!> @details All user-specified expressions must be written in standard
+!! Fortran 90 or in the Fortran version available on your machine. <br>
+!! This routine will be executed after each iteration. <br>
+!! See the list of variables for the use of this routine.
+      SUBROUTINE useite(itout)
+!=======================================================================
+!.....contact fire@avl.com
+!-----
+!-----------------------------------------------------------------------
+!-----
+      USE comm0
+      USE comm1
+      USE comm2
+      USE constants
+!
+      IMPLICIT NONE
+      integer :: i, j, ib,jb, ip1,ip2, ir, mat
+
+!
+!-----parameters
+      INTEGER, INTENT(IN) :: itout  !< current outer iteration index
+!-----
+!-----------------------------------------------------------------------
+!-----
+      IF(I_USEITE == 1) THEN
+
+        mat = 1
+
+!----- Initialise mass fluxes
+!         u(1,1:ncell) = 3.0d0 !2.0d+1
+        CALL initfl(mat)
+!-----        
+
+!         do i=1, nface
+!           WRITE(*,'(a,i10,es10.2)') "USEite: Internal faces ",i, F(i)
+!         enddo
+        DO ir = 0,nreg
+!           WRITE(*,'(a,4i)')"useite: ir:",ir, ibc(1,ir),
+!      x                      ibc(2,ir),ibc(5,ir)
+          IF (ibc(2,ir) .NE. mat) CYCLE
+!           IF (ibc(1,ir) .EQ. 12 .AND. ibc(5,ir) .GT. 1) THEN
+          IF (ibc(1,ir) .EQ. 12) THEN
+            DO ib = nsr(ir),ner(ir)
+              jb = lbj(ib)
+              ip1 = lb(ib)
+              ip2 = lb(jb)
+              fb(ib)=denb(ib)*(ub(1,ib)*sb(1,ib)+ub(2,ib)*sb(2,ib)+
+     *                       ub(3,ib)*sb(3,ib))
+!               fb(jb) = - fb(ib)
+!               WRITE(*,'(a,4i,7es10.2)') "useini bond: ",ib, jb, ip1,ip2
+!      x  , xp(1,ip1),xp(1,ip2), FB(ib), FB(jb),ub(1,ib),u(1,ip1),denb(ib)
+!            CALL rotate(db(:,jb),dbrot(:))
+!            dbrot(:) = db(:,ib)-dbrot(:)
+!            wf1 = SQRT(DOT_PRODUCT(db(:,jb),db(:,jb)) / DOT_PRODUCT(dbrot,dbrot))
+            ENDDO
+          ENDIF
+        ENDDO
+
+!-----
+      ELSE IF(I_USEITE == 2) THEN
+!-----
+      ELSE IF(I_USEITE == 99) THEN
+         CALL USE_FORMULA('USEITE', 1, itout, 0, 0)
+      END IF
+!-----
+      RETURN
+      END SUBROUTINE useite
